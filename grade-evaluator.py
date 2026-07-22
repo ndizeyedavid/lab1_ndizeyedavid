627ac8a7c0ccde86f1e72fcb0b1c58b4ecd073e9
ndizeyedavid <davidndizeye101@gmail.com>
2026-07-22 16:46:47 +0200

    feat: validating the weights from 40 for summative, 60 for formative and a total of 100

--- grade-evaluator.py
+++ grade-evaluator.py
@@ -4,7 +4,9 @@ import sys
 import os
 
 BG_RED = "\033[41m"
+BG_YELLOW = "\033[43m"
 RESET = "\033[0m"
+BG_GREEN = "\033[42m"
 
 def load_csv_data():
     """
@@ -45,9 +47,37 @@ def evaluate_grades(data):
         if 0 < assignment["score"] > 100:
             invalid_score_assignments.append(assignment["assignment"])
             data.pop(index)
-            print(f"{BG_RED}[WARNING]{RESET} Assignment {assignment['assignment']} has the score {assignment['score']} and it's not in the range of 0-100. Therefore it has been excluded")
+            print(f"{BG_YELLOW}[WARNING]{RESET} Assignment {assignment['assignment']} has the score {assignment['score']} and it's not in the range of 0-100. Therefore it has been excluded")
 
-    # TODO: b) Validate total weights (Total=100, Summative=40, Formative=60)
+    if len(invalid_score_assignments) > 0:
+        print("-" * 70)
+
+    # TODOs: b) Validate total weights (Total=100, Summative=40, Formative=60)
+    weights = {"total": 0.0, "Formative": 0.0, "Summative": 0.0}
+    for assignment in data:
+        weights["total"] += assignment["weight"]
+
+        if assignment["group"] == "Formative":
+            weights["Formative"] += assignment["weight"]
+        elif assignment["group"] == "Summative":
+            weights["Summative"] += assignment["weight"]
+            
+    print(f"Total weight for Formatives: {weights["Formative"]}")
+    print(f"Total weight for Summatives: {weights["Summative"]}")
+    print(f"Total weight for all Assignments: {weights["total"]}")
+
+    if weights["Formative"] != 60:
+        print(f"{BG_RED}ERROR{RESET} This Formative weights are NOT well calibrated")
+        sys.exit(1)
+    elif weights["Summative"] != 40:
+        print(f"{BG_RED}ERROR{RESET} This Summative weights are NOT well calibrated")
+        sys.exit(1)
+    elif weights["total"] != 100:
+        print(f"{BG_RED}ERROR{RESET} This assignment's weights are NOT well calibrated")
+        sys.exit(1)
+    else:
+        print(f"{BG_GREEN}ALL WEIGHTS ARE PERFECTLY CALLIBRATED{RESET}")
+
     # TODO: c) Calculate the Final Grade and GPA
     # TODO: d) Determine Pass/Fail status (>= 50% in BOTH categories)
     # TODO: e) Check for failed formative assignments (< 50%)
