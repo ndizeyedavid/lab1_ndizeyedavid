b086b6bda0ae38b2ff9fe90ea9564d8df777440c
ndizeyedavid <d.ndiyeze@alustudent.com>
2026-07-22 20:30:43 +0200

    ft: completed the organizer.sh script file that archives and setsup a new grades.csv file for new grades entry

--- organizer.sh
+++ organizer.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
+log_timestamp=$(date +"%F - %H:%M:%S")
+
+echo "$log_timestamp Starting the Archiving process" >> organizer.log
 
 if [ ! -d "archive" ]; then
+    echo "$log_timestamp Creating the archive directory" >> organizer.log
+    
     echo "Archive Directory Is not present. Creating it now.....";
     mkdir "archive"
+    sleep 1
+    echo "[OK] Archive dir created successfully"
 else
-    echo "The archive directory arleady exists. Archiving instead";
+    echo "[OK] The archive directory arleady exists. Archiving instead";
 fi
+
+echo "$log_timestamp Recording the current Timestamp" >> organizer.log
+
+timestamp=$(date +"%Y%m%d-%H%M%S")
+echo "[OK] Timestamp Recorded"
+
+echo "$log_timestamp Archived grades.csv to grades-$timestamp.csv" >> organizer.log
+mv "./grades.csv" "./archive/grades-$timestamp.csv"
+echo "[OK] grades.csv file successfully archived(grades-$timestamp.csv)"
+
+echo "$log_timestamp New grades.csv file initiated" >> organizer.log
+touch "./grades.csv"
+echo "[OK] New grades.csv file created..."
+
+echo "$log_timestamp Archiving completed" >> organizer.log
+echo "" >> organizer.log
